cddfdde2c7f6a89e9a67a0453917d863dd6e15c6
mjbp <mick.perkins@stormid.com>
2015-07-16 14:25:19 +0100

    First version

--- js/wall.js
+++ js/wall.js
@@ -39,6 +39,63 @@
                 clearTimeout(id);
             };
     }());
+
+    // easing functions http://goo.gl/5HLl8
+    Math.easeInOutQuad = function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) {
+            return c / 2 * t * t + b;
+        }
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    };
+
+    Math.easeInCubic = function (t, b, c, d) {
+        var tc = (t /= d) * t * t;
+        return b + c * (tc);
+    };
+
+    Math.inOutQuintic = function (t, b, c, d) {
+        var ts = (t /= d) * t,
+        tc = ts * t;
+        return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc);
+    };
+
+    //scrollTo
+    function scrollTo(to, duration, callback) {
+        // because it's so fucking difficult to detect the scrolling element, just move them all
+        function move(amount) {
+            document.documentElement.scrollTop = amount;
+            document.body.parentNode.scrollTop = amount;
+            document.body.scrollTop = amount;
+        }
+        function position() {
+            return document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
+        }
+        var start = position(),
+          change = to - start,
+          currentTime = 0,
+          increment = 20;
+        duration = (typeof (duration) === 'undefined') ? 500 : duration;
+        var animateScroll = function () {
+            // increment the time
+            currentTime += increment;
+            // find the value with the quadratic in-out easing function
+            var val = Math.easeInOutQuad(currentTime, start, change, duration);
+            // move the document.body
+            move(val);
+            // do the animation unless its over
+            if (currentTime < duration) {
+                window.requestAnimationFrame(animateScroll);
+            } else {
+                if (callback && typeof (callback) === 'function') {
+                    // the animation is done so lets callback
+                    callback();
+                }
+            }
+        };
+        animateScroll();
+    }
     
     //throttler
     function throttle(fn, ms) {
@@ -86,26 +143,31 @@
             }
             return this;
         }
-     },
-     attributelist = {
-         add: function(el, attrs) {
-             for(var attr in attrs){
-                 el.setAttribute(attr, attrs[attr]);
-             }
-             return this;
+    },
+    attributelist = {
+        add: function(el, attrs) {
+            for(var attr in attrs){
+                el.setAttribute(attr, attrs[attr]);
+            }
+            return this;
          },
         toggle: function(el, attr) {
             el.setAttribute(attr, el.getAttribute(attr) === 'true' ? false : true);
             return this;
         }
-     };
+    },
+    classNames = {
+        trigger: '.js-wall-trigger',
+        child: '.js-wall-child'
+    };
     
     //Wall item constructor
     function WallItem(el, parent, index) {
         this.index = index;
         this.element = el;
-        this.trigger = el.querySelector('.js-wall-trigger');
-        this.child = el.querySelector('.js-wall-child');
+        this.trigger = el.querySelector(classNames.trigger);
+        this.child = el.querySelector(classNames.child);
+        this.throttledToggle = throttle(this.toggle, 750).bind(this);
     }
     
     WallItem.prototype.init = function(parent) {
@@ -137,40 +199,71 @@
         return this;
     };
     
+    WallItem.prototype.keyFinder = function(e) {
+        var triggerKeys = [13, 32];
+        
+        if(triggerKeys.indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+            this.throttledToggle.call(this);
+        }
+    };
+    
     WallItem.prototype.initListeners = function() {
+        var self = this;
+        
         if(!!this.trigger) {
-            this.trigger.addEventListener('click', this.toggle.bind(this), false);
+            this.trigger.addEventListener('click', this.throttledToggle.bind(this), false);
+            this.trigger.addEventListener('keydown', function(e) {
+                self.keyFinder.call(self, e);
+            }, false);
         }
         if(!!this.child) {
-            this.child.querySelector('.js-wall-button-close').addEventListener('click', this.toggle.bind(this), false);
+            this.child.querySelector('.js-wall-button-close').addEventListener('click', this.throttledToggle.bind(this), false);
             if(this.index !== 0) {
-                this.child.querySelector('.js-wall-button-previous').addEventListener('click', this.parent.items[this.index - 1].toggle.bind(this.parent.items[this.index - 1]), false);
+                this.child.querySelector('.js-wall-button-previous').addEventListener('click', this.parent.items[this.index - 1].throttledToggle.bind(this.parent.items[this.index - 1]), false);
             }
             if(this.index !== this.parent.elements.length - 1) {
-                this.child.querySelector('.js-wall-button-next').addEventListener('click', this.parent.items[this.index + 1].toggle.bind(this.parent.items[this.index + 1]), false);
-                //this.child.querySelector('.js-wall-button-next').addEventListener('click', this.toggle.bind(this.parent.items), false);
+                this.child.querySelector('.js-wall-button-next').addEventListener('click', this.parent.items[this.index + 1].throttledToggle.bind(this.parent.items[this.index + 1]), false);
             }
         }
         return this;
     };
     
     WallItem.prototype.toggle = function() {
-        var animateHeight = function(){
+        var lastHeight = 0,
+            heightCounter = 0,
+            animateHeight = function(){
                 this.parent.toggleRow(this.element, (+this.parent.heightClosed + this.child.offsetHeight +'px'));
-                window.requestAnimationFrame(animateHeight.bind(this));
+                if (lastHeight === this.child.offsetHeight) {
+                    heightCounter++;
+                }
+                if (heightCounter < 100) {
+                    lastHeight = this.child.offsetHeight;
+                    window.requestAnimationFrame(animateHeight.bind(this));
+                }
             };
-        
         //check if it has any content
         if(!this.child){ return; }
     
         
         attributelist.toggle(this.trigger, 'aria-expanded');
         attributelist.toggle(this.child, 'aria-hidden');
-        
+
         if(!!this.parent.element.querySelector('.js-wall-item--on')){
             //if it's not this one that's open, close the other then open this
             if(this.parent.elements.indexOf(this.parent.element.querySelector('.js-wall-item--on')) !== this.index) {
-                this.parent.items[this.parent.elements.indexOf(this.parent.element.querySelector('.js-wall-item--on'))].toggle();
+                var current = this.parent.items[this.parent.elements.indexOf(this.parent.element.querySelector('.js-wall-item--on'))];
+                current.toggle();
+                
+                //if on the same row...
+                if(current.element.offsetTop === this.element.offsetTop) {
+                    classlist.add(current.element, 'js-wall-permanent');
+                    classlist.add(this.element, 'js-wall-permanent');
+                    setTimeout(function(){
+                        classlist.remove(current.element, 'js-wall-permanent');
+                        classlist.remove(this.element, 'js-wall-permanent');
+                    }.bind(this), 760);
+                }
                 
                 setTimeout(function(){
                     this.toggle();
@@ -186,9 +279,10 @@
                 classlist.remove(this.element, 'js-is-animating');
                 this.element.style.height = this.parent.heightClosed + 'px';
                 this.parent.toggleRow(this.element, this.parent.heightClosed + 'px');
-            }.bind(this), 260);
+            }.bind(this), 500);
             
         } else {
+            scrollTo(this.element.offsetTop + 50, 260);
             classlist.toggle(this.element, 'js-wall-item--on');
             setTimeout(function(){
                 window.requestAnimationFrame(animateHeight.bind(this));
@@ -230,7 +324,11 @@
         this.items.forEach(function(item){
             if(classlist.has(item.element, '.js-wall-item--on')) {
                 this.heightOpen = item.element.offsetHeight;
-            } else {
+            } 
+            if(classlist.has(item.element, '.js-wall-permanent')) {
+                this.heightOpen = item.element.offsetHeight;
+            } 
+            else {
                 item.element.style.height = 'auto';
                 if(item.element.offsetHeight > this.heightClosed) {
                     this.heightClosed = item.element.offsetHeight;
